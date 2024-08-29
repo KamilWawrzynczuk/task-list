2706844bc8becea6d3355b1e084341c4eac53dc6
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-08-29 13:55:10 +0200

    Setting up routes, redirect, fallback

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+// set up name for route
+Route::get("/xxx", function () {
+    return 'Hello';
+})->name('hello route');
+
+Route::get('hallo', function () {
+    return redirect()->route('hello route');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+// If route is not a match
+Route::fallback(function () {
+    return 'Still ot somwhere!';
+});
+
+
+// GET
+// POST
+// PUT
+// DELETE
