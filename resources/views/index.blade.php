7a3d667c629717d6c4ae35f35e0d5a12132c7fbe
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-08-29 16:00:53 +0200

    Add layouts template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,23 @@
+@extends('layouts.app')
 
-<div>index balde</div>
-@isset($name)
+@section('title', $task->title)
+
+{{-- Chech if something is not null or undefined --}}
+{{-- @isset($name)
 <div> The name is: {{ $name }}</div>
-@endisset
+@endisset --}}
+
+@section('content')
+    <div>
+        {{-- Another way to check if something is in an array  --}}
+        {{-- @if (count($tasks)) --}}
+            @forelse ($tasks as $task)
+                <div>
+                    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title}}</a>
+                </div>
+            @empty
+                <div>There are no tasks.. </div>
+            @endforelse
+        {{-- @endif --}}
+    </div>
+@endsection
