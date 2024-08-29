abff67e9adbb665326616beeb4719ee5686f6eef
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-08-29 15:14:34 +0200

    Add blade template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+
+<div>index balde</div>
+@isset($name)
+<div> The name is: {{ $name }}</div>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Main Page';
+    return view('index', [
+        'name'=> 'Lao',
+    ]
+ );
 });
 
 // set up name for route
