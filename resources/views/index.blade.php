f551625b767e79492d49b70979e5aae93586df9d
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-08-29 16:17:00 +0200

    Set up docker with MySql and connect to database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
-@section('title', $task->title)
-
+@section('title')
+<h1>The list of tasks</h1>
+@endsection
 {{-- Chech if something is not null or undefined --}}
 {{-- @isset($name)
 <div> The name is: {{ $name }}</div>
 @endisset --}}
-
 @section('content')
     <div>
         {{-- Another way to check if something is in an array  --}}
