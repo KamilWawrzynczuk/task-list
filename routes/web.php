8251f71aac2377fe3635b79c523426be55fdf2be
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-09-03 09:02:59 +0200

    Use Query Builder to get data from model and via artisan tinker

--- routes/web.php
+++ routes/web.php
@@ -16,64 +16,73 @@ class Task
   }
 }
 
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
+// $tasks = [
+//   new Task(
+//     1,
+//     'Buy groceries',
+//     'Task 1 description',
+//     'Task 1 long description',
+//     false,
+//     '2023-03-01 12:00:00',
+//     '2023-03-01 12:00:00'
+//   ),
+//   new Task(
+//     2,
+//     'Sell old stuff',
+//     'Task 2 description',
+//     null,
+//     false,
+//     '2023-03-02 12:00:00',
+//     '2023-03-02 12:00:00'
+//   ),
+//   new Task(
+//     3,
+//     'Learn programming',
+//     'Task 3 description',
+//     'Task 3 long description',
+//     true,
+//     '2023-03-03 12:00:00',
+//     '2023-03-03 12:00:00'
+//   ),
+//   new Task(
+//     4,
+//     'Take dogs for a walk',
+//     'Task 4 description',
+//     null,
+//     false,
+//     '2023-03-04 12:00:00',
+//     '2023-03-04 12:00:00'
+//   ),
+// ];
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-       'tasks'=> $tasks,
+      // we want to get all tasks. We want most recent task first
+      // 'tasks'=> \App\Models\Task::latest()->where('completed', true)->get(), methods from Query Builder
+
+       'tasks'=> \App\Models\Task::latest()->get()
     ]
  );
 })->name('tasks.index');
 
-Route::get('/tasks/{id}' , function ($id) use($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
 
-    if(!$task) {
-        abort(404);
-    }
+// we are using use() when we passing data
+// Route::get('/tasks/{id}' , function ($id) use($tasks) {
+Route::get('/tasks/{id}' , function ($id) {
+    // $task = collect($tasks)->firstWhere('id', $id);
 
-    return view('show', ['task' => $task]);
+    // if(!$task) {
+    //     abort(404);
+    // }
+
+    // find return null if do not find anything. We need to take care of it. We are using findOrFail not Find
+    return view('show', [
+      'task' => \App\Models\Task::findOrFail($id)
+      ]);
 
 })->name('tasks.show');
 
