dea03268ebd9c4c4fe4fa84c3f0be268b9e97d32
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-09-09 09:49:23 +0200

    Use TaskRequest for validation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,4 +13,15 @@ class Task extends Model
     // public function getRouteKeyName() {
     //     return "slug";
     // }
+
+    // this is for security reason. We need to set up to not overwritten for example password or sensitive data with mass assigment
+    protected $fillable = [
+        'title',
+        'description',
+        'long_description'
+    ];
+
+    // you can set wich one are guarded but if you do it then every new field will be by default fillable.
+    // so it is better to just use fillable wothout guarded
+    // protected $guarded = ['id','created_at','updated_at'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
-
+use App\Models\Task;
+use App\Http\Requests\TaskRequest;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -26,8 +12,7 @@ Route::get('/tasks', function () {
     return view('index', [
       // we want to get all tasks. We want most recent task first
       // 'tasks'=> \App\Models\Task::latest()->where('completed', true)->get(), methods from Query Builder
-
-       'tasks'=> \App\Models\Task::latest()->get()
+       'tasks'=> Task::latest()->get()
     ]
  );
 })->name('tasks.index');
@@ -35,8 +20,9 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-// We can use Task and laravel will grab primary key wich is id of task. Then we do not need use findOrFail. Laravel will catch that for us
-Route::get('/tasks/{task}/edit' , function (\App\Models\Task $task) {
+// We can use Task and laravel will grab primary key wich is id of task.
+// Then we do not need use findOrFail. Laravel will catch that for us
+Route::get('/tasks/{task}/edit' , function (Task $task) {
 
   return view('edit', [
     'task' => $task
@@ -44,54 +30,28 @@ Route::get('/tasks/{task}/edit' , function (\App\Models\Task $task) {
 
 })->name('tasks.edit');
 
-
 // we are using use() when we passing data
 // Route::get('/tasks/{id}' , function ($id) use($tasks) {
-Route::get('/tasks/{task}' , function (\App\Models\Task $task) {
-    // $task = collect($tasks)->firstWhere('id', $id);
+Route::get('/tasks/{task}' , function (Task $task) {
 
-    // if(!$task) {
-    //     abort(404);
-    // }
-
-    // find return null if do not find anything. We need to take care of it. We are using findOrFail not Find
     return view('show', [
       'task' => $task
       ]);
 
 })->name('tasks.show');
 
-Route::post('/tasks', function(Request $request) {
-  $data = $request->validate([
-    'title'=> 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required',
-  ]);
+Route::post('/tasks', function(TaskRequest $request) {
 
-  $task = new App\Models\Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->completed = false;
-  $task->save();
+  $task = Task::create( $request->validated() );
 
   return redirect()->route('tasks.show', ['task'=> $task->id])
     ->with('success', 'Task created successfuly!');
 
 })->name('tasks.store');
 
-Route::put('/tasks/{task}', function(\App\Models\Task $task, Request $request) {
-  $data = $request->validate([
-    'title'=> 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required',
-  ]);
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
 
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->completed = false;
-  $task->save();
+  $task->update( $request->validated());
 
   return redirect()->route('tasks.show', ['task'=> $task->id])
     ->with('success', 'Task updated successfuly!');
